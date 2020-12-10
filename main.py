5426a66d20582e341877b5edf79e25d1cab2783a
ACEslava <aeslava1970@gmail.com>
2020-12-09 21:36:56 -0800

    Discord Bot release

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-from query import Query
+import query
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+import asyncio
 
-search = Query(input('Wikipedia Article Search \nInput Wikipedia Page title:'))
-options = input('[0] Search \n[1] Scrape \nPlease choose an option: ')
-while True:
-    if options == '0':
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+bot = commands.Bot(command_prefix='$')
+
+
+
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+@bot.command(
+        name = 'search',
+        help='Add a query after $search to search through the Wikipedia databases. Follow the prompts!',
+        brief='Search for a Wikipedia article'
+    )
+async def search(ctx, *args):
+    if not args:
+        await ctx.send('Enter search query:')
+        try:
+            userquery = await bot.wait_for('message', check=lambda m: m.author == ctx.author, timeout = 30) # 30 seconds to reply
+            userquery = userquery.content
+        except asyncio.TimeoutError:
+            await ctx.send(f'{ctx.author.mention} Error: You took too long. Aborting')
+    else: 
+        userquery = args[0]
+    search = query.Query(userquery)
+    while True:
         result = search.searchwikipedia()
-        break
-    elif options == '1':
-        result = search.articlescrape()
-        break
-    else:
-        print('Please choose a valid option')
-        continue
+        if type(result) is list:
+            embed=discord.Embed(title=f"Titles matching '{userquery}':", description=''.join([f'[{index}]: {value}\n' for index, value in enumerate(result)])) #1st 10 results
+            await ctx.send(embed=embed)
+            await ctx.send('Please choose option')
+            while True:  
+                try:
+                    input = await bot.wait_for('message', check=lambda m: m.author == ctx.author, timeout = 30) # 30 seconds to reply 
+                    input = int(input.content)
+                    search.articletitle = result[input]
+                    search.choice = True
+                    break
+                except asyncio.TimeoutError:
+                    await ctx.send(f'{ctx.author.mention} Error: You took too long. Aborting')
+                    break
+                except:
+                    await ctx.send(f'Sorry, I did not understand that. Please choose option (a number from 0-{len(result)-1})')
+                    pass
+            continue
 
-print(result)
+        else:
+            embed=discord.Embed(title=f'Wikipedia Article: {result.original_title}', description=result.summary, url=result.url)
+            await ctx.send(embed=embed)
+            break
+        
+
+
+
+@bot.command(
+        name = 'scrape',
+        help="WIP",
+        brief="Scrape info from a Wikipedia article (WIP)"
+    )
+
+async def scrape(ctx, *args):
+    await ctx.send('WIP')
+
+bot.run(DISCORD_TOKEN)
--- query.py
+++ query.py
@@ -2,38 +2,22 @@ import wikipedia
 class Query:
     def __init__(self, articletitle=None):
         self.articletitle = articletitle
-
+        self.choice = False
     def searchwikipedia(self):
-        usersearch = self.articletitle
-        if usersearch == None: #Can pass search query string to search if needed
-            usersearch = input('Search for a wikipedia article: ')
-            pass
-
-        result = wikipedia.search(usersearch) #Searches Wikipedia for query
-        
-        if len(result) == 0: #Checks if any results are found.
-            while True:
-                didyoumean = input(f"Did you mean: {wikipedia.suggest(usersearch)}? y/n:")
-                if didyoumean == 'y':
-                    return f"Titles matching '{usersearch}': {wikipedia.search(wikipedia.suggest(usersearch))}"
-                elif didyoumean == 'n':
-                    return f"No results found for '{usersearch}'."
-                else:
-                    print(f"Please input a valid choice (y/n)")
-                    continue
+        if self.choice == False:
+            result = wikipedia.search(self.articletitle) #Searches Wikipedia for query
+            if len(result) == 0: #Checks if any results are found.
+                return f"No results found for '{self.articletitle}'."
+            else: return result
         else:
-            print(f"Titles matching '{usersearch}':") #Gives user options
             while True:
-                for index, value in enumerate(result):
-                    print(f'[{index}]: {value}')
-                choice = int(input("Please choose an article: "))
                 try:
-                    return(f'{wikipedia.summary(result[choice])} \nFor more information, go to {wikipedia.page(result[choice]).url}')
+                    return wikipedia.page(title=self.articletitle)
                 except wikipedia.DisambiguationError as e: #If there is a disambiguation of search
                     e = str(e).split('\n')
-                    print(e.pop(0))
-                    result = e
-                    continue
+                    e.pop(0)
+                    return e
+                    
 
     def articlescrape(self):
         pagetitle = self.articletitle
