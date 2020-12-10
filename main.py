845581a2cc19a9b1d08bb2c17c6cd0b4a823488c
ACEslava <aeslava1970@gmail.com>
2020-12-09 17:03:44 -0800

    Adding features

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from query import Query
 
-search = Query(input('Search for a wikipedia article: '))
-print(search.searchwikipedia())
+search = Query(input('Wikipedia Article Search \nInput Wikipedia Page title:'))
+options = input('[0] Search \n[1] Scrape \nPlease choose an option: ')
+while True:
+    if options == '0':
+        result = search.searchwikipedia()
+        break
+    elif options == '1':
+        result = search.articlescrape()
+        break
+    else:
+        print('Please choose a valid option')
+        continue
+
+print(result)
--- query.py
+++ query.py
@@ -1,32 +1,43 @@
 import wikipedia
 class Query:
-    def __init__(self, usersearch):
-        self.usersearch = usersearch
+    def __init__(self, articletitle=None):
+        self.articletitle = articletitle
 
     def searchwikipedia(self):
-        result = wikipedia.search(self.usersearch)
-        if len(result) == 0:
+        usersearch = self.articletitle
+        if usersearch == None: #Can pass search query string to search if needed
+            usersearch = input('Search for a wikipedia article: ')
+            pass
+
+        result = wikipedia.search(usersearch) #Searches Wikipedia for query
+        
+        if len(result) == 0: #Checks if any results are found.
             while True:
-                didyoumean = input(f"Did you mean: {wikipedia.suggest(self.usersearch)}? y/n:")
+                didyoumean = input(f"Did you mean: {wikipedia.suggest(usersearch)}? y/n:")
                 if didyoumean == 'y':
-                    return f"Titles matching '{self.usersearch}': {wikipedia.search(wikipedia.suggest(self.usersearch))}"
+                    return f"Titles matching '{usersearch}': {wikipedia.search(wikipedia.suggest(usersearch))}"
                 elif didyoumean == 'n':
-                    return f"No results found for '{self.usersearch}'."
+                    return f"No results found for '{usersearch}'."
                 else:
                     print(f"Please input a valid choice (y/n)")
                     continue
         else:
-            print(f"Titles matching '{self.usersearch}':")
+            print(f"Titles matching '{usersearch}':") #Gives user options
             while True:
                 for index, value in enumerate(result):
                     print(f'[{index}]: {value}')
                 choice = int(input("Please choose an article: "))
                 try:
-                    return(f'{wikipedia.summary(result[choice], sentences=2)} \n For more information, go to {wikipedia.page(result[choice]).url}')
-                except wikipedia.DisambiguationError as e:
+                    return(f'{wikipedia.summary(result[choice])} \nFor more information, go to {wikipedia.page(result[choice]).url}')
+                except wikipedia.DisambiguationError as e: #If there is a disambiguation of search
                     e = str(e).split('\n')
                     print(e.pop(0))
                     result = e
                     continue
 
-
+    def articlescrape(self):
+        pagetitle = self.articletitle
+        if pagetitle == None: #Can pass search query string to search if needed
+            pagetitle = Query.searchwikipedia().split('https://en.wikipedia.org/wiki/')[1]
+            pass
+        return wikipedia.WikipediaPage(title=pagetitle)
